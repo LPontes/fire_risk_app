7e1870230c11d217eb5c6cb879e373acafe004c8
LPontes <lucas.pontesm@gmail.com>
2023-09-10 17:09:37 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from apps import incendio, clima
 
-st.set_page_config(page_title="Suzano Geospatial", layout="wide")
+st.set_page_config(page_title="Geospatial clim", layout="wide")
 
 # A dictionary of apps in the format of {"App title": "App icon"}
 # More icons can be found here: https://icons.getbootstrap.com
